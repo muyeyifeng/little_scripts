787060f24cc93498170411616fd7911c3a039245
muyeyifeng <muyeyifeng@163.com>
2023-06-11 03:49:55 +0800

    lede-make

---
+++ mk.sh
@@ -0,0 +1,16 @@
+./scripts/feeds update -a
+#./scripts/feeds install -a
+rm -rf ./fail/*
+date_now=$(date "+%F-%H-%M-%S")
+for f in `find . -maxdepth 1 -name "*.defconfig"` 
+do 
+   #make clean
+   echo $f
+   cp $f defconfig
+   cat defconfig > .config
+   make defconfig
+   make download -j8
+   make -j$(nproc) || make -j1 V=s > ./fail/$f.fail
+   sudo aliyunpan login -RefreshToken=#####################
+   sudo aliyunpan upload ./bin/targets /$date_now
+done ;
---
+++ mkmenu.sh
@@ -0,0 +1,13 @@
+#./scripts/feeds update -a
+#./scripts/feeds install -a
+
+for f in `find . -maxdepth 1 -name "*.defconfig"` 
+do 
+   echo $f
+   cp $f defconfig
+   cat defconfig > .config
+   #make download -j8
+   make defconfig
+   make menuconfig
+   ./scripts/diffconfig.sh > $f
+done ;
